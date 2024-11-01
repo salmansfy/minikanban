77de3f961a079aeb58cfe9a4be87af51645ca458
salmansfy <salman.mehboob@synapsify.tech>
2024-11-01 14:37:50 +0500

    deleted graphql files

--- minikanban-backend-main/backend/graphqls/resolvers/list_resolvers.py
+++
@@ -1,9 +0,0 @@
-from ..handlers.list_handler import ListHandler
-
-def resolve_get_list(id):
-    list = ListHandler()
-    return list.get_list(id)
-
-def resolve_get_all_list():
-    list = ListHandler()
-    return list.get_all_list()
--- minikanban-backend-main/backend/models/list_model.py
+++
@@ -1,8 +0,0 @@
-class ListModel:
-    def __init__(self, id, key, title, sort, created, updated):
-        self.id = id
-        self.key = key
-        self.title = title
-        self.sort = sort
-        self.created = created
-        self.updated = updated
